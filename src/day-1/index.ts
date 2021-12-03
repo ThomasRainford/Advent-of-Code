9d3e497f5e904a143e084880b1e2783f9e3ceafc
Thomas Rainford <thomas@rainfords.net>
2021-12-04 11:40:20 +1300

    Initial commit.

---
+++ src/day-1/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+
+const data: number[] = readFileSync("./src/day-1/input.txt", "utf8")
+  .split("\n")
+  .map((item: string) => parseInt(item));
+
+let increased = 0;
+for (let i = 0; i < data.length; i++) {
+  if (i === 0) continue;
+
+  const previous = data[i - 1];
+  const current = data[i];
+
+  if (previous < current) increased++;
+}
+
+console.log(increased);
