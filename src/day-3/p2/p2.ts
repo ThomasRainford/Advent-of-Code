474f2f57ef2ef717882b31b7473325cb488edd0a
Thomas Rainford <thomas@rainfords.net>
2021-12-04 15:08:16 +1300

    Day 3 - part 2

--- src/day-3/p2/p2.ts
+++ src/day-3/p2/p2.ts
@@ -1,45 +1,53 @@
 import { readFileSync } from "fs";
 
 const readInput = (): number[][] => {
-  const data = readFileSync("./src/day-3/p2/sampleInput.txt", "utf8")
+  const data = readFileSync("./src/day-3/p2/input.txt", "utf8")
     .split("\n")
     .map((item: string) => item.split("").map((item) => parseInt(item)));
   return data;
 };
 
-const mostCommon = (value: number, list: number[]) => {
+const mostCommonOx = (list: number[]) => {
   let zeros = 0;
   let ones = 0;
   list.forEach((num) => (num === 0 ? zeros++ : ones++));
-  return value === 0 ? zeros > ones : ones > zeros;
+  if (zeros === ones) return 1;
+  return zeros > ones ? 0 : 1;
 };
 
-const gamma = (list: number[][]) => {
-  let result: number[] = [];
-  list.forEach((i) => {
-    let r = mostCommon(1, i);
-    r ? result.push(1) : result.push(0);
-  });
-  return result;
-};
-
-const epsilon = (list: number[][]) => {
-  let result: number[] = [];
-  list.forEach((i) => {
-    let r = mostCommon(0, i);
-    r ? result.push(1) : result.push(0);
-  });
-  return result;
+const leastCommonCO2 = (list: number[]) => {
+  let zeros = 0;
+  let ones = 0;
+  list.forEach((num) => (num === 0 ? zeros++ : ones++));
+  if (zeros === ones) return 0;
+  return zeros < ones ? 0 : 1;
 };
 
 export const p2 = () => {
   const data = readInput();
-
   const columns = (m: number[][]) => m[0].map((_, i) => m.map((x) => x[i]));
-
   const list = columns(data);
-  const gammaResult = parseInt(gamma(list).join(""), 2);
-  const epsilonResult = parseInt(epsilon(list).join(""), 2);
 
-  console.log(gammaResult * epsilonResult);
+  let oxData = data;
+  let oxList = list;
+  for (let i = 0; i < oxList.length; i++) {
+    const mc = mostCommonOx(oxList[i]);
+    oxData = oxData.filter((col) => col[i] === mc);
+    oxList = columns(oxData);
+    if (oxData.length === 1) break;
+  }
+
+  let co2Data = data;
+  let co2List = list;
+  for (let i = 0; i < co2List.length; i++) {
+    const mc = leastCommonCO2(co2List[i]);
+    co2Data = co2Data.filter((col) => col[i] === mc);
+    co2List = columns(co2Data);
+    if (co2Data.length === 1) break;
+  }
+
+  const result =
+    parseInt(oxData[0].join(""), 2) * parseInt(co2Data[0].join(""), 2);
+
+  console.log(result);
 };
